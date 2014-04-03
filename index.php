7926c3ba6f6cb553d9350c322f38d9f2eaeba2f9
jprieton <PRG001@PRG001-PC>
2014-04-03 14:50:32 -0430

    Ajuste de grid

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
+<?php get_header() ?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-9">
+			<?php
+			while (have_posts()) {
+				get_template_part('content');
+			}
+			?>
+		</div>
+		<div class="col-md-4">
+			<?php get_sidebar() ?>
+		</div>
+	</div>
+
+</div>
 <?php
-
-get_header();
-
-while (have_posts()) {
-	get_template_part('content');
-}
-
 get_footer();
