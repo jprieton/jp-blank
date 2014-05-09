b2eb8e15e7c8ccdd6f8857ddaef532f725389dd5
jprieton <PRG001@PRG001-PC>
2014-05-09 17:04:23 -0430

    Microdata

--- content.php
+++ content.php
@@ -1,18 +1,20 @@
 <?php the_post() ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope itemtype="http://schema.org/Article">
 
 	<header>
-		<h1><?php the_title() ?></h1>
+		<h1 itemprop="name"><?php the_title() ?></h1>
 	</header>
 
-	<div class="post-content">
+	<div class="post-content" itemprop="articleBody">
 		<?php the_content() ?>
 	</div><!-- .post-content -->
 
 	<footer>
 
 	</footer>
+
 	<meta itemprop="datePublished" content="<?php echo get_the_date('i') ?>">
 	<meta itemprop="author" content="<?php echo esc_attr(get_the_author()) ?>">
+	<meta itemprop="url" content="<?php the_permalink() ?>">
 
 </article><!-- #post-## -->
