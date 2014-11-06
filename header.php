06a1f0e27a1522a580e78c26ab66e156dbb582f5
Javier Prieto <jprieton@gmail.com>
2014-11-06 15:45:19 -0430

    Correccion en la estructura. Eliminada la funcion jpb_canonical_meta (deprecated)

--- header.php
+++ header.php
@@ -1,38 +1,32 @@
 <!doctype html>
 <html <?php language_attributes() ?>>
-		<head>
-				<meta charset="<?php bloginfo('charset') ?>" />
-				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<meta name="description" content="<?php bloginfo('description') ?>" />
+	<head>
+		<meta charset="<?php bloginfo('charset') ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="description" content="<?php bloginfo('description') ?>" />
 
-				<title><?php wp_title() ?></title>
+		<title><?php wp_title() ?></title>
 
-				<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri() ?>/favicon.ico" />
-				<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri() ?>/apple-touch-icon.png" />
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri() ?>/favicon.ico" />
+		<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri() ?>/apple-touch-icon.png" />
 
-				<?php jpb_canonical_meta() ?>
+		<?php wp_head() ?>
+		<!-- Main Theme Styles -->
+		<link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>" />
 
-				<?php wp_head() ?>
-				<!-- Main Theme Styles -->
-				<link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>" />
+		<script>
+      var admin_url = '<?php echo admin_url('admin-ajax.php') ?>';
+		</script>
 
-				<script>
-          var admin_url = <?php echo admin_url('admin-ajax.php') ?>;
-				</script>
+		<?php echo get_option('google-analytics', '') ?>
 
-				<?php
-				$analytics = get_option('google-analytics');
-				echo (!empty($analytics)) ? $analytics : '';
-				?>
+	</head>
 
-		</head>
-
-		<body <?php body_class() ?> itemscope itemtype="http://schema.org/WebPage">
-				<div class="container">
-						<div id="site-header">
-								<a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
-										<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="Logo" rel="logo" class="img-responsive">
-								</a>
-						</div>
-				</div>
+	<body <?php body_class() ?> itemscope itemtype="http://schema.org/WebPage">
+		<div class="container">
+			<div id="site-header">
+				<a href="<?php echo esc_url(home_url('/')); ?>" rel="home">
+					<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="Logo" rel="logo" class="img-responsive">
+				</a>
+			</div>
+		</div>
