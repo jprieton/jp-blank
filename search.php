2063068af9a80b994f66963c883a0b36edd2e8b9
Javier Prieto <jprieton@gmail.com>
2014-11-21 14:10:42 -0430

    Plantilla de búsqueda

--- search.php
+++ search.php
@@ -1,8 +1,19 @@
+<?php get_header() ?>
+<div class="container">
+	<div class="row">
+		<div class="col-md-9">
+			<h2><?php printf(__('Search Results for: %s'), '<span>' . get_search_query() . '</span>'); ?></h2>
+			<?php
+			while (have_posts()) {
+				get_template_part('content');
+			}
+			?>
+		</div>
+		<div class="col-md-3">
+			<?php get_sidebar() ?>
+		</div>
+	</div>
+
+</div>
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+get_footer();
