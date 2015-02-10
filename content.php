89b0337b7b5da2a36333d7c0ef3dc1d7b5b234ad
Your Name <you@example.com>
2015-02-10 00:37:06 -0430

    Hover

--- content.php
+++ content.php
@@ -3,7 +3,7 @@
 
 	<header>
 		<h1 itemprop="name"><?php the_title() ?></h1>
-		<div class="post-date" itemprop="datePublished" content="<?php echo get_the_date('i') ?>"><?php _e('Published on', 'jpb'); ?> <?php echo get_the_date() ?></div>
+		<div class="post-date" itemprop="datePublished" content="<?php echo get_the_date('c') ?>"><?php _e('Published on', 'jpb'); ?> <?php echo get_the_date() ?></div>
 		<?php the_post_thumbnail() ?>
 	</header>
 
